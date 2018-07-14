4cc3082ded805ec17fecee27414cc159e7e802d5
Demon Singur <demonsingur@gmail.com>
2018-07-14 14:37:51 +0000

    sdm660-common: do not build useless packages
    They are compiled as dependencies of ipacm
    
    Change-Id: I72076113083308e26573289923f5d3c00a8a6b44

--- sdm660.mk
+++ sdm660.mk
@@ -208,9 +208,7 @@ PRODUCT_COPY_FILES += \
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
-    IPACM_cfg.xml \
-    libipanat \
-    liboffloadhal
+    IPACM_cfg.xml
 
 # IPC router config
 PRODUCT_COPY_FILES += \
@@ -352,8 +350,7 @@ PRODUCT_COPY_FILES += \
 # Radio
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
-    librmnetctl \
-    libxml2
+    librmnetctl
 
 # Recovery
 PRODUCT_PACKAGES += \
