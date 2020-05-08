e3308fa95ff1a27e192add236d4fe511590585eb
Chirayu Desai <chirayudesai1@gmail.com>
2020-05-09 03:46:56 +0530

    sdm660-common: os_pickup: Don't BUILD_WITHOUT_VENDOR
    * It tries to build stuff in hardware/qcom/sm8150, and that needs
      some vendor libs, but even when those are added, it keeps wanting
      more and more, basically a rabbit-hole.
    
    * This matches AOSP.
    
    Change-Id: I9937be534974e249d370e20997a07eefe6bfcc18

--- qcom-caf/Android.mk
+++ qcom-caf/Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter $(LOCAL_PATH),$(PRODUCT_SOONG_NAMESPACES)),)
+ifneq ($(BUILD_WITHOUT_VENDOR), true)
 include $(call first-makefiles-under,$(LOCAL_PATH))
+endif
 endif
