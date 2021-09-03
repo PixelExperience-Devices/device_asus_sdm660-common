1d89e6155aaa4db2e04fdf40670ac9e3faec62ea
SayanAcharya <SayanAcharya@users.noreply.github.com>
2021-09-04 01:22:37 +0530

    sdm660-common: gps: Checkout From Tag LA.UM.9.12.r1-10800-SMxx50.0
    - Drop Prebuilt GPS Blobs And GPS Hals That Were Already Present
    - Freshly Imported GPS Hal From The Aforementioned Tag
    - Switch to gps.conf, flp.conf From gps/etc/
    - Introduce Network Location Overlays
    - Import Prebuilt GPS Blobs Also From The Aforementioned Tag
    - Build All GPS Related Packages And Dependencies From Source
    
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -156,31 +156,28 @@ PRODUCT_PACKAGES += \
     qcom.fmradio \
     qcom.fmradio.xml
 
-# GPS
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.0-impl-qti \
-    android.hardware.gnss@2.0-service-qti \
-    libbatching \
-    libgeofencing \
-    libgnss \
-    libgps.utils \
-    liblocation_api \
-    libsensorndkbridge \
-    libwifi-hal-ctrl
-
-PRODUCT_PACKAGES += \
-    apdr.conf \
-    gps.conf \
-    flp.conf \
-    izat.conf \
-    lowi.conf \
-    sap.conf \
-    xtwifi.conf
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
+
+# GPS
+$(call inherit-product, $(LOCAL_PATH)/gps/gps_vendor_product.mk)
+
+PRODUCT_PACKAGES += \
+    libsensorndkbridge \
+    libwifi-hal-ctrl
+
+PRODUCT_PACKAGES += \
+    gps.conf \
+    flp.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
+    $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 # HIDL
 PRODUCT_PACKAGES += \
