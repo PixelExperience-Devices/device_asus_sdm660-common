df04420d58d76175ada735fe9c8194351c15a85e
SayanAcharya <SayanAcharya@users.noreply.github.com>
2021-09-03 12:40:47 +0530

    sdm660-common: Checkout sound-trigger version to 2.3
    To support new soundtrigger APIs update HIDL version.
    Update sound trigger HIDL version for SP with kernel
    version 4.19.
    
    Signed-off-by: ElectroPerf <kunmun.devroms@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -26,7 +26,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
