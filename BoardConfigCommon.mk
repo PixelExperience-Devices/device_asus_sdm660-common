3299cdaa332174f4116583ef308da83c3e291316
Tri Vo <trong@google.com>
2022-03-15 21:16:18 +0530

    sdm660-common: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy.
    We are restoring the old behavior of BOARD_PLAT_*_SEPOLICY_DIR that
    extends system sepolicy. To that end, we use another make variable to
    specify product sepolicy.
    
    Bug: 119305624
    Test: m selinux_policy
    Change-Id: I67636b7aaff2bd5ae5309981f9fa2f8ce51edf6f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -154,7 +154,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
