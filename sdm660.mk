ea2abea5a82f50bb7b88c631fdaa938a3cea1a14
pkm774 <mprabhat774@gmail.com>
2022-03-22 22:28:15 +0530

    sdm660-common: Build graphics.allocator @3.0 and @4.0

--- sdm660.mk
+++ sdm660.mk
@@ -126,6 +126,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    android.hardware.graphics.allocator@3.0-impl \
+    android.hardware.graphics.allocator@4.0-impl \
     android.frameworks.displayservice@1.0 \
     android.frameworks.displayservice@1.0.vendor
 
