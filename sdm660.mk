705603dc720926e48a21f30fcdc95e4fe0a448a4
ElectroPerf <kunmun.devroms@gmail.com>
2022-06-26 06:09:02 +0000

    sdm660-common: Build Camera
    - Camera Hal
    - Drop Snap As It Glitches In OSS Hal

--- sdm660.mk
+++ sdm660.mk
@@ -105,6 +105,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.sdm660
+    
+PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0 \
     android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
