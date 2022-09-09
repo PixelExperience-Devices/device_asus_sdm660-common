39f0fcb484bf1b65a038e1571d4d2396da8e1915
Alessandro Astone <ales.astone@gmail.com>
2022-09-09 08:21:51 +0000

    sdm660-common: releasetools: Make re.search work on python3
    TypeError: cannot use a string pattern on a bytes-like object
    
    Change-Id: I9e811f3d170402d23fd0448f1a21496a8c56bf88

--- releasetools.py
+++ releasetools.py
@@ -27,7 +27,7 @@ def IncrementalOTA_Assertions(info):
   return
 
 def AddModemAssertion(info):
-  android_info = info.input_zip.read("OTA/android-info.txt")
+  android_info = info.input_zip.read("OTA/android-info.txt").decode('UTF-8')
   m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
   if m:
     version = m.group(1).rstrip()
